17e79ee34c8873c7a12577bf616c74ff5fb1ad89
jonneldev <jonneldosado.code@gmail.com>
2025-05-31 10:04:18 +0800

    push on new branch DIR-98

--- src/components/PerformanceSpotlights.tsx
+++ src/components/PerformanceSpotlights.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import SpotlightCard from './SpotlightCard'
-import spotlightData from '../data/spotlightData'
-import { v4 as uuidv4 } from 'uuid'
+import React, { useEffect, useState, CSSProperties } from 'react';
+import SpotlightCard from './SpotlightCard';
+import { fetchData } from '../services/fetchData';
+import spotlightData from '../data/spotlightData.json';
+import { v4 as uuidv4 } from 'uuid';
+
+interface Spotlight {
+  image: string;
+  title: string;
+}
 
 export default function PerformanceSpotlights() {
+  const [spotlights, setSpotlights] = useState<Spotlight[]>([]);
 
-    const cardContainer = {
-        display: "flex"
-    }
+  useEffect(() => {
+    fetchData(spotlightData).then((data) => setSpotlights(data));
+  }, []);
+
+  const sectionStyle: CSSProperties = {
+    padding: "2em 4em",
+    backgroundColor: "#000",
+    color: "#fff",
+  };
+
+  const titleStyle: CSSProperties = {
+    fontSize: "24px",
+    fontWeight: "700",
+    marginBottom: "1em",
+  };
+
+  const scrollContainerStyle: CSSProperties = {
+    display: "flex",
+    overflowX: "auto",
+    paddingBottom: "0.5em",
+  };
+
   return (
-    <div>
-      <h2>Performance Spotlights</h2>
-      <div style={cardContainer}>
-        {
-            spotlightData.map(spotlight => (
-                < SpotlightCard key={uuidv4()} {...spotlight}/>
-            ))
-        }
+    <div style={sectionStyle}>
+      <h2 style={titleStyle}>Performance Spotlights</h2>
+      <div
+        style={scrollContainerStyle}
+        className="hide-scrollbar" // optional: see below for styling
+      >
+        {spotlights.map((spotlight) => (
+          <SpotlightCard key={uuidv4()} {...spotlight} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/SpotlightCard.tsx
+++ src/components/SpotlightCard.tsx
@@ -1,59 +1,65 @@
-import React, { CSSProperties  } from 'react'
+import React, { CSSProperties } from 'react';
 
 interface SpotlightCardProps {
-    image: string;
-    title: string;
+  image: string;
+  title: string;
 }
 
 export default function SpotlightCard({ image, title }: SpotlightCardProps) {
+  const cardStyle: CSSProperties = {
+    flex: "0 0 auto", 
+    width: "282px",
+    height: "390px",
+    marginRight: "1em",
+    position: "relative",
+    borderRadius: "16px",
+    overflow: "hidden",
+    cursor: "pointer",
+  };
 
-    const cardStyle: CSSProperties = {
-        width: "282px",
-        height: "390px",
-        marginRight: "15px",
-        position: "relative"
-    }
+  const imageStyle: CSSProperties = {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    display: "block",
+  };
 
-    const imageStyle: CSSProperties = {
-        width: "100%",
-        height: "100%",
-        position: "relative"
-        
-    }
+  const buttonStyle: CSSProperties = {
+    backgroundColor: "#000000",
+    width: "48px",
+    height: "48px",
+    borderRadius: "50%",
+    border: "none",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.8,
+  };
 
-    const buttonStyle: CSSProperties = {
-        backgroundColor: "#000000",
-        width: "48px",
-        height: "48px",
-        borderRadius: "50%",
-        border: "1.5px #000000",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        paddingLeft: "0.3em"
-    }
-
-    const titleStyle: CSSProperties = {
-        position: "absolute",
-        bottom: "2em",
-        left: "50%",
-        transform: "translateX(-50%)", 
-        width: "100%",
-        textAlign: "center",
-        fontSize: "20px",
-        fontWeight: "700",
-        textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)"
-    }
+  const titleStyle: CSSProperties = {
+    position: "absolute",
+    bottom: "1.5em",
+    left: "50%",
+    transform: "translateX(-50%)",
+    width: "100%",
+    textAlign: "center",
+    fontSize: "18px",
+    fontWeight: 700,
+    color: "white",
+    textShadow: "2px 2px 5px rgba(0, 0, 0, 0.6)",
+  };
 
   return (
     <div style={cardStyle}>
-      <img src={image} alt={title} style={imageStyle}/>
+      <img src={image} alt={title} style={imageStyle} />
       <p style={titleStyle}>{title}</p>
       <button style={buttonStyle}>
-        <img src="/images/Vector.png" alt="" />
+        <img src="/images/Vector.png" alt="Play" width="20" />
       </button>
     </div>
-  )
+  );
 }
-    
